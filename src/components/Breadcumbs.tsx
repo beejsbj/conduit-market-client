3218031c578850b06b3dcf398bd44305d394a346
beejsbj <burooj.bj@gmail.com>
2025-05-14 15:35:50 -0400

    Add breadcrumbs component
    Signed-off-by: beejsbj <burooj.bj@gmail.com>

---
+++ src/components/Breadcumbs.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Button from './Buttons/Button'
+import { cn } from '@/lib/utils'
+
+interface BreadcrumbItem {
+  label: string
+  path: string
+  isActive?: boolean
+}
+
+interface BreadcrumbsProps {
+  items: BreadcrumbItem[]
+}
+
+const Breadcrumbs: React.FC<BreadcrumbsProps> = ({ items }) => {
+  return (
+    <nav className="flex items-center">
+      {items.map((item, index) => (
+        <React.Fragment key={item.path}>
+          <Button
+            variant="link"
+            size="md"
+            isLink
+            to={item.path}
+            disabled={item.isActive}
+          >
+            {item.label}
+          </Button>
+          {index < items.length - 1 && (
+            <span className="text-gray-400 font-bold">/</span>
+          )}
+        </React.Fragment>
+      ))}
+    </nav>
+  )
+}
+
+export default Breadcrumbs
