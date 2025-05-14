d79b454e1e7ebca2c1fb36f49a5024ad997a6d81
beejsbj <burooj.bj@gmail.com>
2025-05-14 15:35:59 -0400

    Add collection card component
    Signed-off-by: beejsbj <burooj.bj@gmail.com>

---
+++ src/components/Cards/CollectionCard.tsx
@@ -0,0 +1,80 @@
+import type { NDKEvent } from '@nostr-dev-kit/ndk'
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from './CardComponents.tsx'
+import Button from '../Buttons/Button.tsx'
+import UserCounter from '../UserCounter.tsx'
+import { ZapButton } from '../Buttons/ZapButton.tsx'
+import UserAvatar from '../UserAvatar.tsx'
+const PLACEHOLDER_IMAGE =
+  'https://placehold.co/600x400?text=Collection%20Card&font=poppins'
+
+interface CollectionCardProps {
+  event: NDKEvent
+}
+
+const CollectionCard: React.FC<CollectionCardProps> = ({ event }) => {
+  // Placeholder values - these should be extracted from the event later #todo
+  const collectionTitle = 'Collection title in max 2 lines even if it’s short'
+  const collectionAuthor = 'Collection author'
+  const collectionAuthorImage = null
+  const collectionImages = [
+    PLACEHOLDER_IMAGE,
+    PLACEHOLDER_IMAGE,
+    PLACEHOLDER_IMAGE,
+    PLACEHOLDER_IMAGE
+  ]
+  const collectionSlug = 'collection-slug'
+
+  return (
+    <Card className="w-full max-w-sm overflow-hidden relative">
+      {/* card header with usercounter and zap button */}
+      <CardHeader className="grid gap-2">
+        <div className="flex items-center justify-between">
+          <UserCounter showZap={true} size="md" />
+          <ZapButton />
+        </div>
+        <CardTitle className="firm-voice line-clamp-1">
+          {collectionTitle}
+        </CardTitle>
+      </CardHeader>
+
+      {/* card content */}
+      <CardContent className="">
+        <ul className="grid grid-cols-2 gap-2">
+          {collectionImages.map((image, index) => (
+            <li key={index}>
+              <picture className="aspect-video rounded-lg">
+                <img
+                  src={image}
+                  alt={`Collection image ${index + 1}`}
+                  className="w-full h-full object-cover"
+                />
+              </picture>
+            </li>
+          ))}
+        </ul>
+      </CardContent>
+
+      {/* Footer with article collection avatar and read button */}
+      <CardFooter className="flex justify-between items-center ">
+        <UserAvatar name={collectionAuthor} imageUrl={collectionAuthorImage} />
+        <Button
+          variant="secondary"
+          rounded={false}
+          size="lg"
+          to={`/collection/${collectionSlug}`}
+          isLink
+        >
+          Shop
+        </Button>
+      </CardFooter>
+    </Card>
+  )
+}
+
+export default CollectionCard
