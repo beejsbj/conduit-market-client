3947f28f8dfd9b4d842a8fb86bcbb9937e0397fc
beejsbj <burooj.bj@gmail.com>
2025-05-31 21:47:28 -0400

    Add merchant-specific cart detail page
    Signed-off-by: beejsbj <burooj.bj@gmail.com>

--- src/pages/CartDetailPage.tsx
+++ src/pages/CartDetailPage.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useParams } from 'wouter'
+import { useCartStore } from '@/stores/useCartStore'
 
 const CartDetailPage: React.FC = () => {
+  const { merchantPubkey } = useParams()
+
+  const { getCart } = useCartStore()
+  const cart = getCart(merchantPubkey as string)
+
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold">Single Cart Page</h1>
