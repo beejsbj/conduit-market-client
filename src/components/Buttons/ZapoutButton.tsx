300b049fe3dc01bea6ae3978c01635b76e80e371
beejsbj <burooj.bj@gmail.com>
2025-06-02 21:10:10 -0400

    Replace Lucide icon with Icon component in ZapoutButton
    Signed-off-by: beejsbj <burooj.bj@gmail.com>

--- src/components/Buttons/ZapoutButton.tsx
+++ src/components/Buttons/ZapoutButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Button from './Button'
 import type { ButtonProps } from './Button'
-import { Zap } from 'lucide-react'
+import Icon from '../Icon'
 
 export interface ZapoutButtonProps extends Omit<ButtonProps, 'isLink' | 'to'> {}
 
@@ -25,7 +25,7 @@ const ZapoutButton: React.FC<ZapoutButtonProps> = ({
       rounded={rounded}
     >
       <picture>
-        <Zap />
+        <Icon icon="zap" />
       </picture>
 
       {children}
