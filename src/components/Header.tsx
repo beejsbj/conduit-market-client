e949c6b674e60e9c0d8da57248457250dde07c21
beejsbj <burooj.bj@gmail.com>
2025-06-02 21:10:49 -0400

    Refactor Header with Icon component and add Breadcrumbs
    Signed-off-by: beejsbj <burooj.bj@gmail.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,18 +2,13 @@ import React, { useEffect, useRef, useState } from 'react'
 import { useAccountStore } from '@/stores/useAccountStore'
 import Button from './Buttons/Button'
 import useWindowState, { WindowTypes } from '@/stores/useWindowState'
-import {
-  MessagesSquare,
-  SearchIcon,
-  ShoppingCart,
-  User,
-  Wand
-} from 'lucide-react'
+import Icon from './Icon'
 import { useCartStore } from '@/stores/useCartStore'
-import OrderPageButton from './Buttons/OrderPageButton'
 import Field from './Form/Field'
 import Logo from './Logo'
 import MobileMenu from './MobileMenu'
+import PageSection from '@/layouts/PageSection'
+import Breadcrumbs from '@/components/Breadcumbs'
 
 const Header: React.FC = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false)
@@ -84,26 +79,29 @@ const Header: React.FC = () => {
 
   return (
     <header className="relative">
-      <div className="inner-column wide">
+      <PageSection width="wide">
         <div className="flex justify-between items-center gap-4">
-          <Logo className="max-w-50" />
+          <div>
+            <Logo className="max-w-50" />
+            <Breadcrumbs />
+          </div>
 
           {/* Desktop Navigation */}
           <div className="hidden lg:flex items-center flex-1 justify-end gap-4">
             <Button variant="ghost" isLink to="/shop">
-              <ShoppingCart />
+              <Icon icon="shoppingCart" />
               <span className="">Shop</span>
             </Button>
 
             {/* how it works button if logged in else orders page */}
             {isLoggedIn ? (
               <Button variant="ghost" isLink to="/orders">
-                <Wand />
+                <Icon icon="wand" />
                 <span className="">Orders</span>
               </Button>
             ) : (
               <Button variant="ghost" isLink to="/orders">
-                <Wand />
+                <Icon icon="wand" />
                 <span className="">How it works</span>
               </Button>
             )}
@@ -111,32 +109,32 @@ const Header: React.FC = () => {
             <Field
               name="search"
               type="search"
-              rightIcon={<SearchIcon />}
+              rightIcon="search"
               className="w-full"
             />
 
             {/* messages button if logged in */}
             {isLoggedIn && (
               <Button variant="ghost" isLink to="/shop">
-                <MessagesSquare />
+                <Icon icon="messagesSquare" />
                 <span className="">Messages</span>
               </Button>
             )}
 
             {/* sell button */}
             <Button variant="ghost" isLink to="/shop">
-              <Wand />
+              <Icon icon="wand" />
               <span className="">Sell</span>
             </Button>
 
             {/* login button, else user button */}
             {isLoggedIn ? (
               <Button variant="ghost" size="icon" isLink to="/user">
-                <User />
+                <Icon icon="user" />
               </Button>
             ) : (
-              <Button variant="primary" isLink to="/login">
-                <User />
+              <Button variant="primary" isLink to="/auth">
+                <Icon icon="user" />
                 <span className="">Login</span>
               </Button>
             )}
@@ -145,7 +143,7 @@ const Header: React.FC = () => {
           {/* Mobile Menu */}
           <MobileMenu />
         </div>
-      </div>
+      </PageSection>
     </header>
   )
 }
